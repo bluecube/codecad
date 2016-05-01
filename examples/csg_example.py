345584a99cf001177c1cd429569300cede3f45de
Kuba Marek <blue.cube@seznam.cz>
2016-05-02 00:01:59 +0200

    Example tweaks.

--- examples/csg_example.py
+++ examples/csg_example.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import codecad
+import os
 
 sphere = codecad.Sphere(1.3)
 cube = codecad.Box()
@@ -10,5 +11,5 @@ holes = cylinder + cylinder.rotated(1, 0, 0, 90) + cylinder.rotated(0, 0, 1, 90)
 
 o = sphere & cube - holes
 
-#o.render(codecad.rendering.RayCaster("/tmp/csg_example.png"))
-o.render(codecad.rendering.StlRenderer("/tmp/csg_example.stl", 0.01))
+#o.render(codecad.rendering.RayCaster(os.path.splitext(__file__)[0] + ".png"))
+o.render(codecad.rendering.StlRenderer(os.path.splitext(__file__)[0] + ".stl", 0.05))
