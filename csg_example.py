d08943e4ea8e00d09fec862493891fdf124557b0
Kuba Marek <blue.cube@seznam.cz>
2016-05-01 23:38:29 +0200

    Renamed test to CSG example, tweaked values.

--- csg_example.py
+++ csg_example.py
@@ -2,9 +2,9 @@
 
 import codecad
 
-sphere = codecad.Sphere(1.55)
+sphere = codecad.Sphere(1.3)
 cube = codecad.Box()
-cylinder = codecad.Cylinder(d = 0.5, h=2)
+cylinder = codecad.Cylinder(d = 0.4, h=2)
 
 holes = cylinder + cylinder.rotated(1, 0, 0, 90) + cylinder.rotated(0, 0, 1, 90)
 
