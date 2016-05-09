a28770491de4e000ba4c7a836469c59c773f09d5
Kuba Marek <blue.cube@seznam.cz>
2016-05-10 00:07:00 +0200

    Secret project fixes and correct blending of struts.

--- examples/secret_project.py
+++ examples/secret_project.py
@@ -6,42 +6,51 @@ import math
 
 def mesh(outline, spacing, diameter, rounding = None):
     if rounding is None:
-        rounding = diameter
+        rounding = 1.5 * diameter
 
     cylinder = codecad.Cylinder(d=diameter, h=float("inf"))
 
-    grids = []
+    grid_list = []
 
     z_grid = codecad.unsafe.Repetition(cylinder, (spacing, spacing, None))
-    grids.append(z_grid)
-    grids.append(z_grid.rotated((0, 1, 0), 90))
-    grids.append(z_grid.rotated((1, 0, 0), 90))
+    grid_list.append(z_grid)
+    grid_list.append(z_grid.rotated((0, 1, 0), 90))
+    grid_list.append(z_grid.rotated((1, 0, 0), 90))
 
-    xz_grid1 = codecad.unsafe.Repetition(cylinder.rotated((0, 1, 0), 45),
-                                         (2 * spacing, 2 * spacing, None))
+    xz_grid1 = codecad.unsafe.Repetition(cylinder,
+                                         (math.sqrt(2) * spacing, 2 * spacing, None)
+                                         ).rotated((0, 1, 0), 45)
     xz_grid = xz_grid1 + xz_grid1.translated(spacing, spacing, 0)
-    grids.append(xz_grid)
-    grids.append(xz_grid.rotated((0, 1, 0), 90))
+    grid_list.append(xz_grid)
+    grid_list.append(xz_grid.rotated((0, 1, 0), 90))
 
     xy_grid = xz_grid.rotated((1, 0, 0), 90)
-    grids.append(xy_grid)
-    grids.append(xy_grid.rotated((0, 0, 1), 90))
+    grid_list.append(xy_grid)
+    grid_list.append(xy_grid.rotated((0, 0, 1), 90))
 
     yz_grid = xz_grid.rotated((0, 0, 1), 90)
-    grids.append(yz_grid)
-    grids.append(yz_grid.rotated((1, 0, 0), 90))
+    grid_list.append(yz_grid)
+    grid_list.append(yz_grid.rotated((1, 0, 0), 90))
 
-    return outline & codecad.Union(grids, rounding)
+
+    grids = codecad.Union(grid_list)
+
+    if rounding > 0:
+        spheres = codecad.unsafe.Repetition(codecad.Sphere(rounding), (spacing, spacing, spacing))
+        grids = codecad.Union([grids, spheres], rounding)
+
+    return outline & grids
 
 
 outline = codecad.Box(35e-3)
+outline = codecad.Box(20e-3).translated(7.5e-3, 7.5e-3, 7.5e-3)
+outline = codecad.Sphere(100e-3)
 
 o = mesh(outline,
          spacing  = 15e-3,
-         diameter = 1e-3,
-         rounding = 0)
+         diameter = 1e-3)
 
-o.render(codecad.rendering.RayCaster(os.path.splitext(__file__)[0] + ".png",
-         mode=codecad.rendering.RayCaster.dot,
-         resolution=0.1e-3))
-#o.render(codecad.rendering.StlRenderer(os.path.splitext(__file__)[0] + ".stl", 0.5e-3))
+#o.render(codecad.rendering.RayCaster(os.path.splitext(__file__)[0] + ".png",
+#         mode=codecad.rendering.RayCaster.dot,
+#         resolution=0.1e-3))
+o.render(codecad.rendering.StlRenderer(os.path.splitext(__file__)[0] + ".stl", 0.5e-3))
