bd13bff5317a84c5a726dff71e53564399106cac
Kuba Marek <blue.cube@seznam.cz>
2016-06-19 17:32:22 +0200

    No need for the stl of csg example.

--- examples/csg_example.py
+++ examples/csg_example.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 
+""" Replicating the shape from wikipedia's CSG page.
+https://en.wikipedia.org/wiki/Constructive_solid_geometry#/media/File:Csg_tree.png """
+
 import codecad
 
 sphere = codecad.Sphere(1.3)
 cube = codecad.Box()
 cylinder = codecad.Cylinder(d = 0.4, h=2)
 
-holes = cylinder.scaled(1.5) + \
+holes = cylinder + \
         cylinder.rotated((1, 0, 0), 90) + \
-        cylinder.rotated((0, 1, 0), 90).translated(0, 0.2, 0)
+        cylinder.rotated((0, 1, 0), 90)
 
 o = sphere & cube - holes
 
