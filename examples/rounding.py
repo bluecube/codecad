38f9a4ea7ea9265e56d5427cb8e3d0e6c8ff899e
Kuba Marek <blue.cube@seznam.cz>
2016-07-15 20:57:26 +0200

    Rounding example has both rounded and unrounded object.

--- examples/rounding.py
+++ examples/rounding.py
@@ -4,8 +4,10 @@ import codecad
 b1 = codecad.Box(50, 100, 100)
 b2 = codecad.Box(20, 80, 50).rotated((0, 0, 1), 45).translated(20, 0, 0)
 
-o = codecad.Union([b1, b2], 20)
-#o = codecad.Union([b1, b2])
+o1 = codecad.Union([b1, b2])
+o2 = codecad.Union([b1, b2], 20)
+
+o = o1.translated(-50, 0, 0) + o2.translated(50, 0, 0)
 
 if __name__ == "__main__":
     codecad.commandline_render(o, 1, default_renderer="slice")
