22248e30052ad191211dae48eb5c3802204d2c3e
Kuba Marek <blue.cube@seznam.cz>
2016-10-14 20:38:14 +0200

    Better "building expression" measurement for ray caster

--- codecad/rendering/ray_caster.py
+++ codecad/rendering/ray_caster.py
@@ -38,33 +38,33 @@ def render_picture(obj, filename, size = (800, 600),
                              size[1] / 2 - ys)
     directions = directions.normalized()
 
-    ox = T.matrix("ox")
-    oy = T.matrix("oy")
-    oz = T.matrix("oz")
-
-    dx = T.matrix("dx")
-    dy = T.matrix("dy")
-    dz = T.matrix("dz")
-
-    def _trace_func(previous, _, ox, oy, oz, dx, dy, dz):
-        o = util.Vector(ox, oy, oz)
-        d = util.Vector(dx, dy, dz)
-
-        distance = obj.distance(o + d * previous)
-
-        return [previous + 0.8 * distance, distance], \
-               theano.scan_module.until(T.all(T.or_(distance < epsilon / 2,
-                                                    T.isinf(distance))))
-
-    distance, final_value = theano.scan(_trace_func,
-                                        outputs_info=[T.zeros_like(dx),T.zeros_like(dx)],
-                                        non_sequences=[ox, oy, oz, dx, dy, dz],
-                                        n_steps = 100)[0]
-
-    distance = distance[-1]
-    final_value = final_value[-1]
-
     with util.status_block("building expression"):
+        ox = T.matrix("ox")
+        oy = T.matrix("oy")
+        oz = T.matrix("oz")
+
+        dx = T.matrix("dx")
+        dy = T.matrix("dy")
+        dz = T.matrix("dz")
+
+        def _trace_func(previous, _, ox, oy, oz, dx, dy, dz):
+            o = util.Vector(ox, oy, oz)
+            d = util.Vector(dx, dy, dz)
+
+            distance = obj.distance(o + d * previous)
+
+            return [previous + 0.8 * distance, distance], \
+                   theano.scan_module.until(T.all(T.or_(distance < epsilon / 2,
+                                                        T.isinf(distance))))
+
+        distance, final_value = theano.scan(_trace_func,
+                                            outputs_info=[T.zeros_like(dx),T.zeros_like(dx)],
+                                            non_sequences=[ox, oy, oz, dx, dy, dz],
+                                            n_steps = 100)[0]
+
+        distance = distance[-1]
+        final_value = final_value[-1]
+
         r, g, b = mode_func(obj,
                             distance, final_value, epsilon,
                             util.Vector(ox, oy, oz),
