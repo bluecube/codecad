f4d3441d2d5938f16883bd5b087dd2c35e6ff317
Kuba Marek <blue.cube@seznam.cz>
2016-10-31 20:38:53 +0100

    Better renderer registration

--- codecad/rendering/__init__.py
+++ codecad/rendering/__init__.py
@@ -3,47 +3,63 @@ import os
 import argparse
 import importlib
 
-def commandline_render(shape, resolution, default_renderer="guess"):
+import PIL.Image
+
+def commandline_render(shape, resolution, default_renderer=None):
     """ Reads commandline arguments, chooses a renderer and passes the parameters to it. """
 
     parser = argparse.ArgumentParser(description='Render an object')
-    parser.add_argument('--output', '-o', default="output.png",
+    parser.add_argument('--output', '-o',
                         help='File name of the output.')
-    parser.add_argument('--renderer', '-r', default=default_renderer, choices=_renderers,
+    parser.add_argument('--renderer', '-r', choices=_renderers,
                         help='Renderer to use.')
     args = parser.parse_args()
 
-    print("Rendering with renderer {} to file {}".format(args.renderer, args.output))
-    _renderers[args.renderer](shape,
-                              filename=args.output,
-                              resolution=resolution)
-
-def _guess(shape, filename, resolution):
-    if filename.lower().endswith(".stl"):
-        r = "stl"
+    if args.renderer is not None:
+        renderer = args.renderer
     else:
-        r = "picture"
+        renderer = default_renderer
 
-    print("Guessed renderer {}".format(r))
+    if args.output is not None:
+        output = args.output
+        if renderer is None:
+            renderer = _extensions[os.path.splitext(output)[1]]
+    else:
+        if renderer is None:
+            renderer = "picture"
+        ext = _renderers[renderer][1]
+        if ext is not None:
+            output = "output" + ext
 
-    _renderers[r](shape,
-                  filename=filename,
-                  resolution=resolution)
+    if output is not None:
+        print("Rendering with renderer {} to file {}".format(renderer, output))
+    else:
+        print("Rendering with renderer {}".format(renderer))
 
-_modules = {"picture": "ray_caster",
-            "stl": "stl_renderer",
-            "slice": "matplotlib_slice",
-            "gif": "ray_caster"}
+    _renderers[renderer][0](shape, filename=output, resolution=resolution)
 
-_renderers = {"guess": _guess}
-
-for name, module_name in _modules.items():
+def _register(name, module_name, extensions, default_extension = None):
     try:
         module = importlib.import_module("." + module_name, __name__)
     except ImportError as e:
         print("Renderer {} is unavailable due to import error: {}".format(name, str(e)))
-        continue
+        return
+
+    if default_extension is None and len(extensions):
+        default_extension = extensions[0]
+
+    for extension in extensions:
+        _extensions[extension] = name
 
     setattr(sys.modules[__name__], module_name, module)
-    _renderers[name] = getattr(module, "render_" + name)
+    _renderers[name] = (getattr(module, "render_" + name), default_extension)
 
+_renderers = {}
+_extensions = {}
+
+PIL.Image.init()
+
+_register("picture", "ray_caster", PIL.Image.EXTENSION.keys(), ".png")
+_register("stl", "stl_renderer", [".stl"])
+_register("slice", "matplotlib_slice", [])
+_register("gif", "ray_caster", [".gif"])
