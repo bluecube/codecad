7b2f856d734ec032ec0faea2e22c10de53d5e150
Kuba Marek <blue.cube@seznam.cz>
2016-12-04 17:04:38 +0100

    Test fix

--- tests/test_naca_airfoil.py
+++ tests/test_naca_airfoil.py
@@ -6,6 +6,6 @@ def check_parameters(param_obj, thickness, max_camber, max_camber_position):
     codecad.util.check_close(param_obj.max_camber_position, max_camber_position, 0.001)
 
 def test_parameters():
-    check_parameters(codecad.naca_airfoil.NacaAirfoil("2412"), 0.12, 0.02, 0.4)
-    check_parameters(codecad.naca_airfoil.NacaAirfoil("0015"), 0.15, 0.0, 0.0)
+    check_parameters(codecad.shapes.naca_airfoil.NacaAirfoil("2412"), 0.12, 0.02, 0.4)
+    check_parameters(codecad.shapes.naca_airfoil.NacaAirfoil("0015"), 0.15, 0.0, 0.0)
 
