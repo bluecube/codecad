683c7179de975b26b55ff4b7d0ee5be4687a0f76
Kuba Marek <blue.cube@seznam.cz>
2016-12-22 00:24:42 +0100

    Setting some opencl compile options

--- codecad/compute/compute.py
+++ codecad/compute/compute.py
@@ -1,4 +1,6 @@
 import struct
+import warnings
+
 import pyopencl
 
 from . import codegen
@@ -14,7 +16,15 @@ queue = pyopencl.CommandQueue(ctx)
 #a_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=a_np)
 #b_g = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=b_np)
 
-program = pyopencl.Program(ctx, codegen.collect_program()).build()
+with warnings.catch_warnings():
+    # We compile with Werror, so there is no need for python warnings here
+    warnings.simplefilter("ignore")
+    program = pyopencl.Program(ctx, codegen.collect_program()).build(
+        options=["-Werror",
+                 "-cl-single-precision-constant",
+                 "-cl-denorms-are-zero",
+                 "-cl-no-signed-zeros",
+                 "-cl-fast-relaxed-math"])
 
 #res_g = cl.Buffer(ctx, mf.WRITE_ONLY, a_np.nbytes)
 #prg.sum(queue, a_np.shape, None, a_g, b_g, res_g)
