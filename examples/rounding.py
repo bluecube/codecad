2b1587718c876dfe186d0b5ca136a5532a252015
Kuba Marek <blue.cube@seznam.cz>
2017-01-09 20:04:40 +0100

    Converted the rounding example to use 2d shapes

--- examples/rounding.py
+++ examples/rounding.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import codecad
 
-b1 = codecad.shapes.box(50, 100, 100)
-b2 = codecad.shapes.box(20, 80, 50).rotated((0, 0, 1), 45).translated(20, 0, 0)
+b1 = codecad.shapes.rectangle(50, 100)
+b2 = codecad.shapes.rectangle(20, 80).rotated(45).translated(20, 0)
 
 o1 = codecad.shapes.union([b1, b2])
 o2 = codecad.shapes.union([b1, b2], 20)
 
-o = o1.translated(-50, 0, 0) + o2.translated(50, 0, 0)
+o = o1.translated(-50, 0) + o2.translated(50, 0)
 
 if __name__ == "__main__":
     codecad.commandline_render(o, 0.5, default_renderer="slice")
