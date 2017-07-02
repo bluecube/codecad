7206093c083e69da4f151a6866f6bed2d1cd10dd
Kuba Marek <blue.cube@seznam.cz>
2017-07-02 22:49:53 +0200

    STL renderer using the mesh generator

--- codecad/rendering/stl_renderer.py
+++ codecad/rendering/stl_renderer.py
@@ -1,27 +1,23 @@
 import numpy
-import mcubes
 import stl.mesh
 
-from ..compute import grid_eval
+from . import mesh
 from .. import util
 
 def render_stl(obj, filename, resolution):
-    obj.check_dimension(required = 3)
+    with util.status_block("generating mesh"):
+         pieces = list(mesh.triangular_mesh(obj, resolution))
+    triangle_count = sum(len(piece[1]) for piece in pieces)
 
-    with util.status_block("running"):
-        values, corner, step = grid_eval.grid_eval(obj, resolution)
-
-    values = numpy.transpose(values, (1, 2, 0))
-
-    with util.status_block("marching cubes"):
-        vertices, triangles = mcubes.marching_cubes(values, 0)
-
-    with util.status_block("exporting {} triangles".format(len(triangles))):
-        mesh = stl.mesh.Mesh(numpy.empty(triangles.shape[0], dtype=stl.mesh.Mesh.dtype))
-        for i, f in enumerate(triangles):
-            mesh.vectors[i][0] = step * vertices[f[1],:]
-            mesh.vectors[i][1] = step * vertices[f[0],:]
-            mesh.vectors[i][2] = step * vertices[f[2],:]
+    with util.status_block("exporting {} triangles".format(triangle_count)):
+        stl_mesh = stl.mesh.Mesh(numpy.zeros(triangle_count, dtype=stl.mesh.Mesh.dtype))
+        i = 0
+        for vertices, indices in pieces:
+            for triangle in indices:
+                for j, vertex in enumerate(triangle):
+                    v = vertices[vertex]
+                    stl_mesh.vectors[i, j] = v
+                i += 1
 
     with util.status_block("saving"):
-        mesh.save(filename)
+        stl_mesh.save(filename)
