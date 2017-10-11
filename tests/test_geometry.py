2cb09fbe0dd42803a75482b9ee3737e06d23a3b3
Kuba Marek <blue.cube@seznam.cz>
2017-10-11 12:34:59 +0200

    Refactored geometry tests a bit, added test for transform * 0 and 0 * transform

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -6,33 +6,51 @@ from pytest import approx
 import codecad
 from codecad.util.geometry import Vector, Quaternion, Transformation
 
-
-@pytest.mark.parametrize("transformation, v, target", [
-    (Transformation.zero(), (9, 8, 7), (9, 8, 7)),
-    (Transformation.from_degrees((0, 0, 1), 0, 1, (0, 0, 0)), (2, 3, 4), (2, 3, 4)),
+quaternions_to_test = [
     (Quaternion.from_degrees((0, 0, 1), 90, 1), (1, 1, 1), (-1, 1, 1)),
     (Quaternion.from_degrees((0, 0, 1), 90, 2), (1, 1, 1), (-2, 2, 2)),
     (Quaternion.from_degrees((0, 1, 0), -45, 2) * Quaternion.from_degrees((0, 0, 1), 45, 0.5),
         (1, 0, 0), (0.5, math.sqrt(0.5), 0.5)),
+    ]
+quaternions_to_test = [(t, codecad.util.Vector(*a), codecad.util.Vector(*b)) for (t, a, b) in quaternions_to_test]
+
+transformations_to_test = [
+    (Transformation.zero(), (9, 8, 7), (9, 8, 7)),
+    (Transformation.from_degrees((0, 0, 1), 0, 1, (0, 0, 0)), (2, 3, 4), (2, 3, 4)),
     (Transformation.from_degrees((0, 0, 1), 90, 1, (5, 5, 5)), (1, 1, 1), (4, 6, 6)),
     (Transformation.from_degrees((0, 0, 1), 90, 2, (1, 0, 1)), (1, 1, 1), (-1, 2, 3)),
     (Transformation.from_degrees((1, 0, 0), 0, 1, (1, 0, 0)) * Transformation.from_degrees((0, 0, 1), 90, 1, (0, 0, 0)),
         (0, 0, 0), (1, 0, 0)),
     (Transformation.from_degrees((0, 0, 1), 90, 1, (0, 0, 0)) * Transformation.from_degrees((1, 0, 0), 0, 1, (1, 0, 0)),
-        (0, 0, 0), (0, 1, 0)),
-])
-def test_transformation(transformation, v, target):
-    v = codecad.util.Vector(*v)
-    target = codecad.util.Vector(*target)
+        (0, 0, 0), (0, 1, 0))
+]
+transformations_to_test = [(t, codecad.util.Vector(*a), codecad.util.Vector(*b)) for (t, a, b) in transformations_to_test]
 
-    transformed = transformation.transform_vector(v)
-    assert transformed == approx(target)
+all_to_test = quaternions_to_test + transformations_to_test
 
-    inv_transformation = transformation.inverse()
-    assert inv_transformation.transform_vector(transformed) == approx(v)
 
-    zero_transformation = inv_transformation * transformation
-    assert zero_transformation.transform_vector(v) == approx(v)
+@pytest.mark.parametrize("transformation, v, target", all_to_test)
+def test_vector_transformation(transformation, v, target):
+    assert transformation.transform_vector(v) == approx(target)
 
+
+@pytest.mark.parametrize("transformation, v, target", all_to_test)
+def test_vector_transformation_inverse(transformation, v, target):
+    assert transformation.inverse().transform_vector(target) == approx(v)
+
+
+@pytest.mark.parametrize("transformation, v, target", all_to_test)
+def test_vector_transformation_inverse_times_original(transformation, v, target):
+    assert (transformation.inverse() * transformation).transform_vector(v) == approx(v)
+
+
+@pytest.mark.parametrize("transformation, v, target", all_to_test)
+def test_vector_transformatio_matrix(transformation, v, target):
     matrix_transformed = transformation.as_matrix() * v.as_matrix()
     assert codecad.util.Vector(*matrix_transformed.A1[:3]) == approx(target)
+
+
+@pytest.mark.parametrize("transformation, v, target", all_to_test)
+def test_vector_transformation_times_zero(transformation, v, target):
+    assert (transformation * transformation.zero()).transform_vector(v) == approx(target)
+    assert (transformation.zero() * transformation).transform_vector(v) == approx(target)
