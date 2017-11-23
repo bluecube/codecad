ed64ad5b0cd87a4140bd19d6ccb31612da3fe8df
Kuba Marek <blue.cube@seznam.cz>
2017-11-23 11:11:10 +0100

    Headers in BOM export

--- codecad/rendering/bom.py
+++ codecad/rendering/bom.py
@@ -6,5 +6,7 @@ def render_bom(obj, filename, resolution):
     # Resolution is unused
 
     with open(filename, "w") as fp:
-        csv.writer(fp).writerows([item.name, item.count] + item.part.attributes
-                                 for item in obj.assembly().bom())
+        writer = csv.writer(fp)
+        writer.writerow(["name", "count"])
+        writer.writerows([item.name, item.count] + item.part.attributes
+                         for item in obj.assembly().bom())
