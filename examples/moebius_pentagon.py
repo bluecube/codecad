aff236f2ecd371b3e761b98cb1e592ecd9c5c3f3
Kuba Marek <blue.cube@seznam.cz>
2017-12-08 13:15:05 +0100

    Added alignment pins to moebius pentagon example

--- examples/moebius_pentagon.py
+++ examples/moebius_pentagon.py
@@ -4,10 +4,28 @@ import codecad
 from codecad.shapes import *
 import math
 
-mp = regular_polygon2d(5, d=30).revolved(r=25, twist=360/5)
+minor_r = 15
+major_r = 25
+pin_d = 5
+pin_h = 10
+second_pin_angle = 170  # Not 180 degrees to have only one way to assemble
+
+mp = regular_polygon2d(5, r=minor_r).revolved(r=major_r, twist=360/5)
+
+pin_hole = cylinder(d=1.1 * pin_d, h=1.2 * pin_h).rotated_x(90).translated_x(major_r)
+mp -= pin_hole
+mp -= pin_hole.rotated_y(second_pin_angle)
+
 half1 = (mp & half_space()).rotated_x(90).make_part("half1")
 half2 = (mp - half_space()).rotated_x(-90).make_part("half2")
-asm = codecad.Assembly([half1, half2.rotated_x(180)])
+
+pin = cylinder(d=pin_d, h=pin_h, symmetrical=False).make_part("pin")
+pin = pin.translated_z(-pin_h/2).translated_x(major_r)
+
+asm = codecad.Assembly([half1,
+                        half2.rotated_x(180),
+                        pin,
+                        pin.rotated_z(second_pin_angle)])
 
 if __name__ == "__main__":
     codecad.commandline_render(asm, 0.5)
