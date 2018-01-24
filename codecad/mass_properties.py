ad6ab258619f401454b9f626408542c2f60aabfc
Kuba Marek <blue.cube@seznam.cz>
2018-01-24 21:29:49 +0100

    Another mass properties WIP.
    This time it looks fairly precise, but impractically slow

--- codecad/mass_properties.py
+++ codecad/mass_properties.py
@@ -28,31 +28,33 @@ class MassProperties(collections.namedtuple("MassProperties", "volume centroid i
     __slots__ = ()
 
 
-def mass_properties(shape, resolution, grid_size=None):
+def mass_properties(shape, precision=1e-4):
+    """ Calculate volume, centroid and inertia tensor of the shape.
+    Iteratively subdivides the shape until
+    abs(actual_volume - computed_volume) < precision * actual_volume """
     # Inertia tensor info:
     # http://farside.ph.utexas.edu/teaching/336k/Newtonhtml/node64.html
+    #
+    # Some useful integrals:
+    # integral(x, a, a + s, integral(y, b, b + s, integral(z, c, c + s, x))) = s**3 * (a + s / 2)
+    # integral(x, a, a + s, integral(y, b, b + s, integral(z, c, c + s, x * y))) = s**3 * (a + s / 2) * (b + s / 2)
 
-    if grid_size is None:
-        # TODO: Determine default grid size
-        grid_size = INNER_LOOP_SIDE * 32
+    grid_size = INNER_LOOP_SIDE * 32  # Max allowed grid size
+    initial_grid_size = INNER_LOOP_SIDE * 16  # Only to cut away large chunks of empty space
 
     assert shape.dimension() == 3, "2D objects are not supported yet"
-    assert resolution > 0, "Non-positive resolution makes no sense"
-    assert grid_size > 2, "Grid needs to be > 2"
+    assert grid_size > 2, "Grid side size needs to be > 2"
     assert grid_size % INNER_LOOP_SIDE == 0, "Grid size must be divisible by {}".format(INNER_LOOP_SIDE)
-    assert 0.5 * grid_size**5 <= 2**32, "Centroid coordinate sums would overflow uint32"
+    assert grid_size < 2**8, "Grid coordinates must fit uint8"
+    assert 0.25 * grid_size**3 * (grid_size + 1)**2 <= 2**32, \
+        "Centroid coordinate sums must fit uint32"
 
     program_buffer = nodes.make_program_buffer(shape)
 
     box = shape.bounding_box()
-    block_sizes = subdivision.calculate_block_sizes(box,
-                                                    shape.dimension(),
-                                                    resolution,
-                                                    grid_size,
-                                                    overlap=False,
-                                                    level_size_multiplier=INNER_LOOP_SIDE)
-    block_sizes = [(resolution * cell_size, level_size)
-                   for cell_size, level_size in block_sizes]
+    box_size = box.size()
+    initial_step = box_size.max() / initial_grid_size
+    initial_grid = [int(util.round_up_to(s / initial_step, INNER_LOOP_SIDE)) for s in box_size]
 
     integral_one = util.KahanSummation()
     integral_x = util.KahanSummation()
@@ -74,7 +76,10 @@ def mass_properties(shape, resolution, grid_size=None):
                  integral_xy, integral_xz, integral_yz
         nonlocal kernel_invocations, function_evaluations
 
-        box_corner, level = job_id
+        current_corner, current_step, current_grid, current_allowed_error = job_id
+        assert all(x <= grid_size for x in current_grid)
+
+        current_evaluations = functools.reduce(operator.mul, current_grid)
 
         index_sums = cl_util.Buffer(numpy.uint32, 10, pyopencl.mem_flags.READ_WRITE)
         intersecting_counter = cl_util.Buffer(numpy.uint32, 1, pyopencl.mem_flags.READ_WRITE)
@@ -82,32 +87,25 @@ def mass_properties(shape, resolution, grid_size=None):
                                            grid_size**3,
                                            pyopencl.mem_flags.WRITE_ONLY)
 
-        box_step = block_sizes[level][0]
-        grid_dimensions = block_sizes[level][1]
-        assert all(x <= grid_size for x in grid_dimensions)
-        assert all(x % INNER_LOOP_SIDE == 0 for x in grid_dimensions)
-        shifted_corner = box_corner + util.Vector.splat(box_step / 2)
-        if level < len(block_sizes) - 1:
-            distance_threshold = box_step * math.sqrt(3) / 2
-        else:
-            distance_threshold = 0
-
         # Enqueue write instead of fill to work around pyopencl bug #168
         fill_ev = index_sums.enqueue_write(numpy.zeros(10, index_sums.dtype))
         fill_ev = intersecting_counter.enqueue_write(numpy.zeros(1, intersecting_counter.dtype), wait_for=[fill_ev])
 
-        global_work_size = [x // INNER_LOOP_SIDE for x in grid_dimensions]
-        #print(box_corner, box_step, grid_dimensions, global_work_size)
+        shifted_corner = current_corner + util.Vector.splat(current_step / 2)
+        distance_threshold = current_step * math.sqrt(3) / 2
+
+        assert all(x % INNER_LOOP_SIDE == 0 for x in current_grid)
+        global_work_size = [x // INNER_LOOP_SIDE for x in current_grid]
 
         yield opencl_manager.k.mass_properties(global_work_size, None,
                                                program_buffer,
-                                               shifted_corner.as_float4(), numpy.float32(box_step),
+                                               shifted_corner.as_float4(), numpy.float32(current_step),
                                                numpy.float32(distance_threshold),
                                                index_sums,
                                                intersecting_counter, intersecting_list,
                                                wait_for=[fill_ev])
         kernel_invocations += 1
-        function_evaluations += functools.reduce(operator.mul, grid_dimensions)
+        function_evaluations += current_evaluations
 
         intersecting_count = intersecting_counter.read()[0]
         intersecting_event = intersecting_list.enqueue_read()
@@ -115,7 +113,7 @@ def mass_properties(shape, resolution, grid_size=None):
         # For all the functions in question, convert `sum f(I)` (where I are indices
         # of occupied cells) to `integral f(X)` over all occupied cells.
 
-        s = block_sizes[level][0]
+        s = current_step
         s2 = s * s
         s3 = s * s2
         b = shifted_corner
@@ -145,14 +143,52 @@ def mass_properties(shape, resolution, grid_size=None):
         integral_xz += s3 * (n * b.x * b.z + b.x * tmp_z + b.z * tmp_x + tmp_xz)
         integral_yz += s3 * (n * b.y * b.z + b.y * tmp_z + b.z * tmp_y + tmp_yz)
 
-        level = level + 1
-        assert level < len(block_sizes) or intersecting_count == 0
+        # The adaptive part:
+        # Everything in this part is calculated in blocks instead of in volumes
+        # (Everything is divided by s3).
+        # TODO: The precision setting will not work correctly on mostly empty
+        # models (eg. a grid of tiny balls at vertices of the initial grid)
+        if current_allowed_error is None:
+            # Handle first kernel launch
+            estimate = n + intersecting_count / 2  # Volume integral estimate for this block
+            current_allowed_error = precision * estimate
+        error_bound = intersecting_count / 2
 
         intersecting_event.wait()
-        return ((util.Vector(i, j, k) * s + box_corner, level)
-                for i, j, k, l in intersecting_list[:intersecting_count])
+        sub_block_corners = (util.Vector(i, j, k) * s + current_corner
+                           for i, j, k, l in intersecting_list[:intersecting_count])
+        if error_bound < current_allowed_error:
+            # The error is small, we can stop subdividing
 
-    cl_util.interleave2(job, [(box.a, 0)])
+            # Add the intersecting sub blocks to the integral with 0.5 weight
+            for sub_block_corner in sub_block_corners:
+                weight = 0.5 * s3
+
+                sub_block_center = sub_block_corner + util.Vector.splat(s)
+                integral_one += weight
+                integral_x +=  weight * sub_block_center.x
+                integral_y +=  weight * sub_block_center.y
+                integral_z +=  weight * sub_block_center.z
+                integral_xx += weight * sub_block_center.x**2
+                integral_yy += weight * sub_block_center.y**2
+                integral_zz += weight * sub_block_center.z**2
+                integral_xy += weight * sub_block_center.x * sub_block_center.y
+                integral_xz += weight * sub_block_center.x * sub_block_center.z
+                integral_yz += weight * sub_block_center.y * sub_block_center.z
+
+            # No more jobs from here
+            return []
+        else:
+            # Must subdivide
+            next_step = current_step / grid_size
+            next_grid = [grid_size,] * 3
+            next_allowed_error = current_allowed_error * grid_size**3 / intersecting_count
+
+            next_jobs = [(next_corner, next_step, next_grid, next_allowed_error)
+                         for next_corner in sub_block_corners]
+            return next_jobs
+
+    cl_util.interleave2(job, [(box.a, initial_step, initial_grid, None)])
 
     # Unwrap the integral values from the KahanSummation objects
     integral_one = integral_one.result
@@ -168,9 +204,9 @@ def mass_properties(shape, resolution, grid_size=None):
 
     # Relative speedup compared to just blindly dividing the bounding box to the
     # final resolution in one step
-    direct_evaluations = functools.reduce(operator.mul, (box.size() / resolution).applyfunc(math.ceil))
-    speedup = 1 - function_evaluations / direct_evaluations
-    print("speedup:", speedup)
+    #direct_evaluations = functools.reduce(operator.mul, (box.size() / resolution).applyfunc(math.ceil))
+    #speedup = 1 - function_evaluations / direct_evaluations
+    #print("speedup:", speedup)
 
     volume = integral_one
     if volume == 0:
