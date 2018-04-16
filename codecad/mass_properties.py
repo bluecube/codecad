5b3b7ab8d608018679f3ad6aebad072428e5eb68
Kuba Marek <blue.cube@seznam.cz>
2018-04-16 18:46:27 +0200

    Plane split fudge factor heuristic now considers tree size
    ... for smoother potential switch to octrees

--- codecad/mass_properties.py
+++ codecad/mass_properties.py
@@ -73,7 +73,7 @@ def mass_properties(shape,
         plane_split_fudge_factor = cltypes.float("inf")
     else:
         # Chose empirically, no real basis for this expression :-)
-        plane_split_fudge_factor = cltypes.float(0.1 / shape.feature_size())
+        plane_split_fudge_factor = cltypes.float(2.5 / (shape.feature_size() * TREE_SIZE**3))
 
     with cl_util.BufferList() as buffer_list:
         program_buffer = nodes.make_program_buffer(shape)
