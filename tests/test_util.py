9b642e02e48fc12e909e8f9a80f65ca8b9cdc8ce
Kuba Marek <blue.cube@seznam.cz>
2018-10-13 22:51:18 +0200

    Helpers for handling vectors from outside of codecad.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,7 @@
 import codecad
 import hypothesis
 import pytest
+import decimal
 
 
 def test_kahan_summation():
@@ -26,3 +27,51 @@ def test_kahan_summation():
 @hypothesis.given(hypothesis.strategies.lists(hypothesis.strategies.booleans()))
 def test_at_most_one(l):
     assert codecad.util.at_most_one(l) == (sum(l) <= 1)
+
+
+class NumberWrapper:
+    def __init__(self, v):
+        self.v = v
+
+    def __float__(self):
+        return float(self.v)
+
+
+@hypothesis.given(
+    hypothesis.strategies.lists(
+        hypothesis.strategies.integers()
+        | hypothesis.strategies.floats()
+        | hypothesis.strategies.decimals()
+        | hypothesis.strategies.fractions()
+        | hypothesis.strategies.floats().map(lambda x: NumberWrapper(x))
+        | hypothesis.strategies.floats().map(lambda x: str(x)),
+        min_size=2,
+        max_size=3,
+    )
+)
+def test_wrap_vector_like1(value):
+    codecad.util.types.wrap_vector_like(value)
+
+
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (codecad.util.Vector(1, 2, 3), codecad.util.Vector(1, 2, 3)),
+        ((1, 2, 3), codecad.util.Vector(1, 2, 3)),
+        ((4, 5), codecad.util.Vector(4, 5, 0)),
+        (
+            (decimal.Decimal("1e5"), NumberWrapper(7), 2),
+            codecad.util.Vector(decimal.Decimal("1e5"), 7.0, 2),
+        ),
+        (range(2), codecad.util.Vector(0, 1, 0)),
+        (range(3), codecad.util.Vector(0, 1, 2)),
+    ],
+)
+def test_wrap_vector_like2(value, expected):
+    assert codecad.util.wrap_vector_like(value) == expected
+
+
+@pytest.mark.parametrize("value", [[], (1, 2, 3, 4), (4,), 6, "X"])
+def test_wrap_vector_like_fail(value):
+    with pytest.raises((TypeError, ValueError)):
+        codecad.util.wrap_vector_like(value)
