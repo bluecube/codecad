dd4ad73bc1b375d3e117a347ac8c260325543f7b
Kuba Marek <blue.cube@seznam.cz>
2018-12-06 21:53:06 +0100

    Started codecad math module

---
+++ codecad/math.py
@@ -0,0 +1,30 @@
+""" Trig functions working in degrees rather than radians. """
+
+import math
+
+def cos(deg):
+    return math.cos(math.radians(deg))
+
+
+def sin(deg):
+    return math.sin(math.radians(deg))
+
+
+def tan(deg):
+    return math.tan(math.radians(deg))
+
+
+def acos(v):
+    return math.degrees(math.acos(v))
+
+
+def asin(v):
+    return math.degrees(math.asin(v))
+
+
+def atan(v):
+    return math.degrees(math.atan(v))
+
+
+def atan2(y, x):
+    return math.degrees(math.atan2(y, x))
